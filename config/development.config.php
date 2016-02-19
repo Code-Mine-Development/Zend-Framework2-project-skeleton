56e8baacc14dad46a1bd7a92bffba2d1048386e6
Radek Adamiec <radek@code-mine.com>
2016-02-19 10:07:22 +0100

    Added CommandQuery generator to development conf

--- config/development.config.php
+++ config/development.config.php
@@ -4,16 +4,17 @@
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
-return array(
+return [
     // Development time modules
-    'modules' => array(
+    'modules'                 => [
         'ZFTool',
         'ZF\Apigility\Admin',
-    ),
+        'CodeMine\CommandQueryGenerator',
+    ],
     // development time configuration globbing
-    'module_listener_options' => array(
-        'config_glob_paths' => array('config/autoload/{,*.}{global,local}-development.php'),
-        'config_cache_enabled' => false,
+    'module_listener_options' => [
+        'config_glob_paths'        => ['config/autoload/{,*.}{global,local}-development.php'],
+        'config_cache_enabled'     => false,
         'module_map_cache_enabled' => false,
-    )
-);
+    ],
+];
